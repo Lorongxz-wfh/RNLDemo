53e628ee90dbeba5da5afd8c2efaab81e1b87ff8
Lorongxz-wfh <lorongxz.wfh@gmail.com>
2025-08-09 19:32:13 +0800

    Part 17 (Thirteenth Commit)

--- client/src/pages/User/components/UserList.tsx
+++ client/src/pages/User/components/UserList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type FC } from "react";
+import { useCallback, useEffect, useRef, useState, type FC } from "react";
 import {
   Table,
   TableBody,
@@ -14,26 +14,44 @@ interface UserListProps {
   onAddUser: () => void;
   onEditUser: (user: UserColumns | null) => void;
   onDeleteUser: (user: UserColumns | null) => void;
-  refreshKey: boolean
+  refreshKey: boolean;
 }
 
-const UserList: FC<UserListProps> = ({ onAddUser, onEditUser, onDeleteUser, refreshKey }) => {
+const UserList: FC<UserListProps> = ({
+  onAddUser,
+  onEditUser,
+  onDeleteUser,
+  refreshKey,
+}) => {
   const [loadingUsers, setLoadingUsers] = useState(false);
   const [users, setUsers] = useState<UserColumns[]>([]);
+  const [usersTableCurrentPage, setUsersTableCurrentPage] = useState(1);
+  const [usersTableLastPage, setUsersTableLastPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
-  const handleLoadUsers = async () => {
+  const tableRef = useRef<HTMLDivElement>(null);
+
+  const handleLoadUsers = async (page: number, append = false) => {
     try {
       setLoadingUsers(true);
 
-      const res = await UserService.loadUsers();
+      const res = await UserService.loadUsers(page);
 
       if (res.status === 200) {
-        setUsers(res.data.users);
+        const usersData = res.data.users.data || res.data.users || [];
+        const lastPage =
+          res.data.users.last_page ||
+          res.data.last_page ||
+          usersTableLastPage ||
+          1;
+
+        setUsers(append ? [...users, ...usersData] : usersData);
+        setUsersTableCurrentPage(page);
+        setUsersTableLastPage(lastPage);
+        setHasMore(page < lastPage);
       } else {
-        console.error(
-          "Unexpected status error occurrred during loading users: ",
-          res.status
-        );
+        setUsers(append ? users : []);
+        setHasMore(false);
       }
     } catch (error) {
       console.error(
@@ -44,6 +62,19 @@ const UserList: FC<UserListProps> = ({ onAddUser, onEditUser, onDeleteUser, refr
       setLoadingUsers(false);
     }
   };
+
+  const handleScroll = useCallback(() => {
+    const ref = tableRef.current;
+
+    if (
+      ref &&
+      ref.scrollTop + ref.clientHeight >= ref.scrollHeight - 10 &&
+      hasMore &&
+      !loadingUsers
+    ) {
+      handleLoadUsers(usersTableCurrentPage + 1, true);
+    }
+  }, [hasMore, loadingUsers, usersTableCurrentPage]);
 
   const handleUserFullNameFormat = (user: UserColumns) => {
     let fullName = "";
@@ -64,13 +95,30 @@ const UserList: FC<UserListProps> = ({ onAddUser, onEditUser, onDeleteUser, refr
   };
 
   useEffect(() => {
-    handleLoadUsers();
+    const ref = tableRef.current;
+
+    if (ref) {
+      ref.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (ref) {
+        ref.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [handleScroll]);
+
+  useEffect(() => {
+    handleLoadUsers(usersTableCurrentPage, false);
   }, [refreshKey]);
 
   return (
     <>
       <div className="overflow-hidden rounded-lg border border-gray-200 bg-white">
-        <div className="max-w-full max-h-[calc(100vh)] overflow-x-auto">
+        <div
+          ref={tableRef}
+          className="relative max-w-full max-h-[calc(100vh-8.5rem)] overflow-x-auto"
+        >
           <Table>
             <caption className="mb-4">
               <div className="border-b border-gray-100">
@@ -127,13 +175,7 @@ const UserList: FC<UserListProps> = ({ onAddUser, onEditUser, onDeleteUser, refr
               </TableRow>
             </TableHeader>
             <TableBody className="divide-y divide-gray-100 text-gray-500 text-sm">
-              {loadingUsers ? (
-                <TableRow>
-                  <TableCell className="px-4 py-3 text-center" colSpan={6}>
-                    <Spinner size="md" />
-                  </TableCell>
-                </TableRow>
-              ) : (
+              {users.length ?? 0 > 0 ? (
                 users.map((user, index) => (
                   <TableRow className="hover:bg-gray-100" key={index}>
                     <TableCell className="px-4 py-3 text-center">
@@ -171,6 +213,19 @@ const UserList: FC<UserListProps> = ({ onAddUser, onEditUser, onDeleteUser, refr
                     </TableCell>
                   </TableRow>
                 ))
+              ) : (
+                <TableRow>
+                  <TableCell className="px-4 py-3 text-center" colSpan={6}>
+                    <Spinner size="md" />
+                  </TableCell>
+                </TableRow>
+              )}
+              {loadingUsers && (users.length ?? 0) > 0 && (
+                <TableRow>
+                  <TableCell className="px-4 py-3 text-center" colSpan={6}>
+                    <Spinner size="md" />
+                  </TableCell>
+                </TableRow>
               )}
             </TableBody>
           </Table>
--- server/app/Http/Controllers/Api/UserController.php
+++ server/app/Http/Controllers/Api/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
     {
         $users = User::with(['gender'])
             ->where('tbl_users.is_deleted', false)
-            ->get();
+            ->paginate(15);
 
         return response()->json([
             'users' => $users
